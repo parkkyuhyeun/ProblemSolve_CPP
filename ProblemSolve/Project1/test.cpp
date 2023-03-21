d94ba9d75fa8535be698ac08904e5615a7cb5437
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-03-21 23:55:56 +0900

    1차원 배열 - 바구니 뒤집기

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <windows.h>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	int n;
-	int* array = new int[30];
-	for (int i = 0; i < 30; i++)
+	int n, m, i, j;
+	cin >> n >> m;
+	int* basket = new int[n];
+	int* re_basket = new int[n];
+	for (int v = 0; v < n; v++)
 	{
-		array[i] = i + 1;
+		basket[v] = v + 1;
+		re_basket[v] = v + 1;
 	}
-	for (int i = 0; i < 28; i++)
+	for (int v = 0; v < m; v++)
 	{
-		cin >> n;
-		for (int i = 0; i < 30; i++)
+		cin >> i >> j;
+		int count = i - 1;
+		for (int g = j - 1; g >= i - 1; g--) //µÚÁı±â
 		{
-			if (array[i] == n) { array[i] = 0; }
+			re_basket[count] = basket[g];
+			count++;
+		}
+		for (int b = 0; b < n; b++)
+		{
+			basket[b] = re_basket[b];
 		}
 	}
-	for (int i = 0; i < 30; i++)
+	for (int t = 0; t < n; t++)
 	{
-		if (array[i] != 0) {
-			cout << array[i] << endl;
-		}
+		cout << re_basket[t] << " ";
 	}
 }
