b1452f7e388fafb66f3a623535d7b3f88abe2601
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-03-23 23:16:06 +0900

    문자열 단계 - 그대로 출력하기

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -4,17 +4,10 @@ using namespace std;
 
 int main()
 {
-	char A[4], B[4], r_A[4], r_B[4];
-	cin >> A >> B;
-	int j = 2, a=0, b=0;
-	for (int i = 0; i < 3; i++)
+	string s;
+	for (int i = 0; i < 100; i++)
 	{
-		r_A[i] = A[j];
-		r_B[i] = B[j];
-		j--;
+		getline(cin, s);
+		cout << s << endl;
 	}
-	a = (r_A[0] - 48) * 100 + (r_A[1] - 48) * 10 + (r_A[2] - 48);
-	b = (r_B[0] - 48) * 100 + (r_B[1] - 48) * 10 + (r_B[2] - 48);
-
-	(a > b ? cout << a : cout << b);
 }
