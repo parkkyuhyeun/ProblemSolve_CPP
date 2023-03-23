0c1b1455576f7954ccfeb4e41b16cb787a9138e9
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-03-23 23:35:53 +0900

    심화 1 - 별찍기-7

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -4,10 +4,30 @@ using namespace std;
 
 int main()
 {
-	string s;
-	for (int i = 0; i < 100; i++)
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		getline(cin, s);
-		cout << s << endl;
+		for (int j = n - 1; j > i; j--)
+		{
+			cout << " ";
+		}
+		for (int l = 0; l <= i*2; l++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << " ";
+		}
+		for (int l = 2*n-5; l >= i*2-1; l--)
+		{
+			cout << "*";
+		}
+		cout << endl;
 	}
 }
