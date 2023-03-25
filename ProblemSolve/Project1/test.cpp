78a263f721ef571325e5bdaadf93ccfa08922547
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-03-25 13:21:56 +0900

    심화 1 - 단어 공부

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 int main()
 {
-	string s;
-	cin >> s;
-	int t_count = 0;
-	for (int i = 0; i < s.size() / 2; i++)
-	{
-		if (s[i] == s[s.size() - i - 1]) t_count++;
-	}
-	if (t_count == s.size() / 2) cout << 1;
-	else cout << 0;
+    string input = "";
+    int arr[26] = { 0 };
+    cin >> input;
+    int max = 0;
+    int num = 0;
+
+    char answer = NULL;
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        input[i] = toupper(input[i]);
+        arr[input[i] - 65]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        num = 0;
+        for (int j = 0; j < input.size(); j++)
+        {
+            if ((int)input[j] - 65 == i)
+                num++;
+        }
+
+        if (max < num)
+        {
+            max = num;
+            answer = char(i + 65);
+        }
+        else if (max == num)
+        {
+            answer = '?';
+        }
+    }
+
+    cout << answer;
 }
