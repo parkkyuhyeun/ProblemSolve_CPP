65ece2fd74f3e330055e507bf3fdfd3de80117f3
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-05-09 11:45:12 +0900

    스택 - 제로

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <stack>
-#include <string>
 
 using namespace std;
 
-int main()
-{
-	int caseCount;
-	string str;
-	cin >> caseCount;
-	for (int i = 0; i < caseCount; i++)
-	{
-		stack<char> st;
-		cin >> str;
-		for (int j = 0; j < str.size(); j++)
-		{
-			if (str[j] == '(') st.push(str[j]);
-			else if (str[j] == ')')
-			{
-				if (st.empty())
-				{
-					st.push('s');
-					break;
-				}
-				st.pop();
-			}
-		}
-		if (st.empty()) cout << "YES" << endl;
-		else cout << "NO" << endl;
-	}
+int main() {
+    int k = 0;
+    int num = 0;
+    int sum = 0;
+    stack<int> st;
+    cin >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> num;
+        if (num != 0)
+        {
+            st.push(num);
+        }
+        else
+        {
+            st.pop();
+        }
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        if (st.empty())
+        {
+            cout << sum;
+            return 0;
+        }
+        else
+        {
+            sum = sum + st.top();
+            st.pop();
+        }
+    }
+
+    cout << sum;
+    return 0;
 }
