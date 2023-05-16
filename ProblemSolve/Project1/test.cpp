6f7f82729bdae825974da75d5dbf84ad6a1da613
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-05-16 11:19:53 +0900

    스택 - 카드2

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
-#include <stack>
-#include <string>
+#include <queue>
 using namespace std;
 
 int main() {
-	int t, cnt = 0;
-	string str;
-	cin >> t;
-	while (t--)
+	int N;
+	queue<int> q;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
 	{
-		stack<char>st;
-		cin >> str;
-		for (int i = 0; i < str.size(); i++)
-		{
-			if (!st.empty() && str[i] == st.top())
-			{
-				st.pop();
-			}
-			else
-			{
-				st.push(str[i]);
-			}
-		}
-		if (st.empty()) cnt++;
+		q.push(i);
 	}
-	cout << cnt;
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front();
 }
