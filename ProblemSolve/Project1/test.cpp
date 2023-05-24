45b97d2871e9e05bc4ba5f305896958151e65274
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-05-24 11:06:07 +0900

    큐, 덱 - AC

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,27 +1,76 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<deque>
+#include<string>
 using namespace std;
 
-int main() {
-	int N, K;
-	queue<int> q;
-	cin >> N >> K;
-	for (int i = 1; i <= N; i++)
-	{
-		q.push(i);
-	}
-	cout << "<";
-	while (q.size() != 0)
-	{
-		for (int i = 1; i < K; i++)
-		{
-			q.push(q.front());
-			q.pop();
-		}
-		cout << q.front();
-		if (q.size() != 1) cout << ", ";
-		q.pop();
-	}
+int main()
+{
+    string order, nums, num;
+    int t, numsize;
+    bool isError = false, isReverse = false;
+    cin >> t;
 
-	cout << ">";
+    for (int i = 0; i < t; ++i)
+    {
+        isError = isReverse = false;
+        deque<int> deq;
+        cin >> order >> numsize >> nums;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= '0' && nums[i] <= '9') {
+                num += nums[i];
+            }
+            else if (nums[i] == ',' || nums[i] == ']') {
+                if (!num.empty()) {
+                    deq.push_back(stoi(num));
+                    num = "";
+                }
+            }
+        }
+
+        for (int j = 0; j < order.size(); ++j)
+        {
+            if (order[j] == 'R')
+                isReverse = !isReverse;
+            else
+            {
+                if (deq.empty())
+                {
+                    isError = true;
+                    break;
+                }
+                if (!isReverse)
+                    deq.pop_front();
+                else
+                    deq.pop_back();
+            }
+        }
+
+        if (isError)
+            cout << "error\n";
+        else if (deq.empty())
+            cout << "[]\n";
+        else if (!isReverse)
+        {
+            cout << '[' << deq.front();
+            deq.pop_front();
+            while (!deq.empty())
+            {
+                cout << ',' << deq.front();
+                deq.pop_front();
+            }
+            cout << "]\n";
+        }
+        else
+        {
+            cout << '[' << deq.back();
+            deq.pop_back();
+            while (!deq.empty())
+            {
+                cout << ',' << deq.back();
+                deq.pop_back();
+            }
+            cout << "]\n";
+        }
+    }
 }
