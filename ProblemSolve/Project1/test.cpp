3d8fd987362bae8c169749e74aa171ec6a4478f8
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-06-20 11:14:39 +0900

    10815_숫자 카드

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
-#include <map>
-#include <string>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
-int n, m;
-unordered_map<int, string> numToString;
-unordered_map<string, int> stringToNum;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> n >> m;
-    string poketmon;
-
-    for (int i = 1; i <= n; i++) {
-        cin >> poketmon;
-        numToString[i] = poketmon;
-        stringToNum[poketmon] = i;
+    unordered_set<int> s;
+    int n, m;
+    cin >> n;
+    int cardNum;
+    for (int i = 0; i < n; i++) {
+        cin >> cardNum;
+        s.insert(cardNum);
     }
-
-    for (int i = 1; i <= m; i++) {
-        cin >> poketmon;
-        if (isdigit(poketmon[0])) {
-            cout << numToString[stoi(poketmon)] << endl;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> cardNum;
+        if (s.find(cardNum) != s.end()) {
+            cout << 1 << " ";
         }
         else {
-            cout << stringToNum[poketmon] << endl;
+            cout << 0 << " ";
         }
     }
 }
