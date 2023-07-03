94aa9bde2d6d653e64ad00619e87d06adf097540
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-07-03 10:32:52 +0900

    1547_공

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
-#include <unordered_set>
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    unordered_set<int> s;
-    int n, m;
-    cin >> n;
-    int cardNum;
-    for (int i = 0; i < n; i++) {
-        cin >> cardNum;
-        s.insert(cardNum);
-    }
+    int m, x, y, temp;
+    int cups[3] = { 1,0,0 };
     cin >> m;
     for (int i = 0; i < m; i++) {
-        cin >> cardNum;
-        if (s.find(cardNum) != s.end()) {
-            cout << 1 << " ";
-        }
-        else {
-            cout << 0 << " ";
+        cin >> x >> y;
+        temp = cups[x-1];
+        cups[x-1] = cups[y-1];
+        cups[y-1] = temp;
+    }
+    for (int i = 0; i < 3; i++) {
+        if (cups[i] == 1) {
+            cout << i + 1;
         }
     }
 }
