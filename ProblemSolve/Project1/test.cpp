7d32a96dcc54dbdefc4518c3affb50aa997ac1d3
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-07-03 10:52:25 +0900

    1269_대칭 차집합

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <unordered_set>
 using namespace std;
 
 int main()
 {
-    int m, x, y, temp;
-    int cups[3] = { 1,0,0 };
-    cin >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> x >> y;
-        temp = cups[x-1];
-        cups[x-1] = cups[y-1];
-        cups[y-1] = temp;
+    int aCount, bCount, answer, count = 0;
+    cin >> aCount >> bCount;
+    unordered_set<int> a;
+    unordered_set<int> b;
+    for (int i = 0; i < aCount; i++) {
+        cin >> answer;
+        a.insert(answer);
     }
-    for (int i = 0; i < 3; i++) {
-        if (cups[i] == 1) {
-            cout << i + 1;
+    for (int i = 0; i < bCount; i++) {
+        cin >> answer;
+        if (a.find(answer) == a.end()) {
+            count++;
+        }
+        b.insert(answer);
+    }
+    for (auto i : a) {
+        if (b.find(i) == b.end()) {
+            count++;
         }
     }
+    cout << count;
 }
