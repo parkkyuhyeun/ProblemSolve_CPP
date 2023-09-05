085c4b0acdc887146927863da2b9a95f2310a48e
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-09-05 16:40:44 +0900

    18352_특정 거리의 도시 찾기

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int n, score = 0, upper = 1;
-    string answer;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> answer;
-        for (int j = 0; j < answer.size(); j++) {
-            if (answer[j] == 'O') {
-                score += upper;
-                upper++;
-            }
-            else if (answer[j] == 'X') {
-                upper = 1;
-            }
-        }
-        cout << score << endl;
-        upper = 1;
-        score = 0;
-    }
+	int n, m, k, x, a, b;
+	vector<vector<int>> adj;
+	cin >> n >> m >> k >> x;
+	vector<int> answer;
+	vector<int> visited;
+	adj.resize(n + 1);
+	visited.resize(n + 1, -1);
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		adj[a].push_back(b);
+	}
+	queue<int> q;
+	q.push(x);
+	visited[x] = 0;
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+		for (int i = 0; i < adj[now].size(); i++){
+			int next = adj[now][i];
+			if (visited[next] == -1) {
+				q.push(next);
+				visited[next] = visited[now] + 1;
+				if (visited[next] == k) {
+					answer.push_back(next);
+				}
+			}
+		}
+	}
+	sort(answer.begin(), answer.end());
+	if (answer.empty()) cout << -1;
+	else {
+		for (auto e : answer) {
+			cout << e << "\n";
+		}
+	}
 }
