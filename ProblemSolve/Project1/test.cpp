ee8b186d950f6985e1ef2d99546333e505517ff5
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-09-26 15:06:33 +0900

    11170_0의 개수

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	int n, ans;
-	cin >> n;
-	for (int i = 0; i < 9; i++) {
-		cin >> ans;
-		n -= ans;
+	int t, n, m;
+	string ns;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int count = 0;
+		cin >> n >> m;
+		for (int j = n; j <= m; j++) {
+			ns = to_string(j);
+			for (int k = 0; k < ns.size(); k++) {
+				if (ns[k] == '0') count++;
+			}
+		}
+		cout << count << endl;
 	}
-	cout << n;
 }
