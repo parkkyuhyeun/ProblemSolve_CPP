3da4834e34249314be035554c8ec6e948c4201bd
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-10-05 17:58:12 +0900

    27323_직사각형

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <string>
+#include <queue>
+#include <vector>
 using namespace std;
 
 int main() {
-	string S;
-	getline(cin, S);
-	for (int i = 0; i < S.size(); i++) {
-		if (S[i] >= 'A' && S[i] <= 'Z') {
-			S[i] += 13;
-			if (S[i] > 90) S[i] -= 26;
-		}
-		if (S[i] >= 'a' && S[i] <= 'z') {
-			S[i] > 109 ? S[i] -= 13 : S[i] += 13;
-		}
-	}
-	std::cout << S;
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << a * b;
 }
