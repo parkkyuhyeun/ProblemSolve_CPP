9cbd2e0317b102a3e120b6800d25d13bc6689d82
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-10-05 18:21:36 +0900

    1085_직사각형에서 탈출

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -4,8 +4,42 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a;
-    cin >> b;
-    cout << a * b;
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+	if (x < y) {
+		if (x < w - x) {
+			if (x < h - y) {
+				cout << x;
+			}
+			else {
+				cout << h - y;
+			}
+		}
+		else {
+			if (w - x < h - y) {
+				cout << w - x;
+			}
+			else {
+				cout << h - y;
+			}
+		}
+	}
+	else {
+		if (y < h - y) {
+			if (y < w - x) {
+				cout << y;
+			}
+			else {
+				cout << w - x;
+			}
+		}
+		else {
+			if (h - y < w - x) {
+				cout << h - y;
+			}
+			else {
+				cout << w - x;
+			}
+		}
+	}
 }
