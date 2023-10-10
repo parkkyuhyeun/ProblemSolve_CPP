a04a7bc2dd50945f8047b6b7ad898e57ca9bf98f
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-10-10 15:24:11 +0900

    1920_수 찾기

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <map>
 #include <vector>
 using namespace std;
 
-int main() {
-	int n, k, a, count = 0;
-	cin >> n >> k;
-	vector<int> v;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		v.push_back(a);
-	}
-	for (int i = n - 1; i >= 0; i--) {
-		count += k / v[i];
-		k -= (k / v[i]) * v[i];
-	}
-	cout << count;
+int main()
+{
+    int N, M, tmp;
+    map<int, bool> mp;
+    vector<int> ans;
+    cin >> N;
+    while (N--)
+    {
+        cin >> tmp;
+        mp[tmp] = true;
+    }
+
+    cin >> M;
+    while (M--)
+    {
+        cin >> tmp;
+        ans.push_back(mp[tmp]);
+    }
+
+    for (int i : ans)
+        cout << i << '\n';
 }
