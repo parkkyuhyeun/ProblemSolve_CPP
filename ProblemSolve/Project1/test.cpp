d29213443c0a8bcf5376ca3b40f40dc0040773e7
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2023-10-13 10:35:54 +0900

    5639_이진 검색 트리

--- ProblemSolve/Project1/test.cpp
+++ ProblemSolve/Project1/test.cpp
@@ -3,25 +3,46 @@
 #include <vector>
 using namespace std;
 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* root = NULL;
+Node* MakeBTreeNode(int value) {
+    Node* nd = new Node;
+    nd->data = value;
+    nd->left = NULL;
+    nd->right = NULL;
+    return nd;
+}
+void InsertRecur(Node* r, Node* node) {
+    if (root == NULL) root = node;
+    else if (node->data < r->data) {
+        if (r->left == NULL) r->left = node;
+        else InsertRecur(r->left, node);
+    }
+    else {
+        if (r->right == NULL) r->right = node;
+        else InsertRecur(r->right, node);
+    }
+}
+void PostOrder(Node* r) {
+    if (r->left != NULL) PostOrder(r->left);
+    if (r->right != NULL) PostOrder(r->right);
+    cout << r->data << endl;
+}
 int main()
 {
-    int N, M, tmp;
-    map<int, bool> mp;
-    vector<int> ans;
-    cin >> N;
-    while (N--)
-    {
-        cin >> tmp;
-        mp[tmp] = true;
-    }
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
 
-    cin >> M;
-    while (M--)
-    {
-        cin >> tmp;
-        ans.push_back(mp[tmp]);
+    int value;
+    while (cin >> value) {
+        //if (value == 0) break;
+        InsertRecur(root, MakeBTreeNode(value));
     }
-
-    for (int i : ans)
-        cout << i << '\n';
+    PostOrder(root);
+    return 0;
 }
